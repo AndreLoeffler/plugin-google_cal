c323ff5c0c32a8ad1868a0e6bf40813f4b9934af
Andre Loeffler <info@andre-loeffler.net>
2013-09-13 11:31:37 +0200

    syntax error

--- syntax.php
+++ syntax.php
@@ -51,7 +51,7 @@ class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
             }
             
             // Only parameter for $disp right now is "a" for Agenda
-            if ($disp == 'a') $disp = 'showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&';
+            if ($disp == 'a') $disp = 'showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;';
             if (!isset($disp)) $disp = '';
             
             if (!isset($alt)) $alt = '';
