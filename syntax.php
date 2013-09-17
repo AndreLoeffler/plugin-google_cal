ae69d2a55329982d0de56d8e411b5dcb8f1c095a
Andre Loeffler <info@andre-loeffler.net>
2013-09-17 10:38:27 +0200

    changed again

--- syntax.php
+++ syntax.php
@@ -74,10 +74,10 @@ class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
             // Two styles: wiki and error
             switch($style) {
                 case 'wiki':
-		    $script = "<iframe src='http://www.google.com/calendar/embed?".$disp."src=$url&amp;height=$h&amp;title=$alt'"."title='$alt'  width='$w' height='$h' frameborder='0'></iframe>";
-                    $renderer->doc .= "<script> document.write(".$script.")<script>\n".
+		    $script = "<iframe src='http://www.google.com/calendar/embed?".$disp."src=$url&amp;height=$h&amp;title=$alt'"." width='$w' height='$h' frameborder='0'></iframe>";
+                    $renderer->doc .= "<script type='text/html'> document.write(".$script.")<script>\n".
 				      "<noscript><iframe src='http://www.google.com/calendar/embed?".$disp."src=$url&amp;height=$h&amp;title=$alt'".
-                "title='$alt'  width='$w' height='$h' frameborder='0'></iframe><noscript>";
+                " width='$w' height='$h' frameborder='0'></iframe><noscript>";
                     break;
                 case 'error':
                     $renderer->doc .= "<div class='error'>$url</div>";
